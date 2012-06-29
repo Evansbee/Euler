6b8bc999d9e1c9b3f7ea08f08fbdb92d5014e0a2
Evan Ackmann <evansbee@gmail.com>
2012-06-28 23:41:21 -0400

    22 done

--- ex22.py
+++ ex22.py
@@ -4,7 +4,7 @@ names = []
 for line in open('ex22.in','r'):
    for name in line.split(','):
       #stupid quotes...
-      names.append(name[1:-1])
+      names.append(name.strip('"'))
 
 names.sort()
 
@@ -15,7 +15,6 @@ for i in range(len(names)):
    for c in names[i]:
       word_score += (ord(c) - ord('A')) + 1
    score = word_score * (i+1)
-   print "%s - w: %d p:%d"%(names[i],word_score,i+1)
    total += score
 
-print score
+print total
