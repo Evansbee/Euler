d6762cf41f724867b761205e504ad380cffec4a3
Evan Ackmann <evansbee@gmail.com>
2012-06-28 22:20:48 -0400

    18 and 67 done

---
+++ ex67.py
@@ -0,0 +1,30 @@
+#just add it downward since we only need the answer, not every option
+#challenge is the data structure -- let's try to read it in intelligently so I don't have to do something stupid in exercise 67
+
+#file is in ex67.in
+
+input = open("ex67.in", 'r')
+
+row, col = 0 , 0
+
+tri = {}
+
+
+#well formated input means we can just dump this baby out
+for line in input:
+   for num in line.split():
+      tri[(row,col)] = int(num)
+      col += 1
+   row += 1
+   col = 0
+
+for i in range(row-2, -1, -1):
+   for j in range(i + 1):
+      o1,o2 = tri[(i+1,j)], tri[(i+1,j+1)] 
+      
+      if o1 > o2:
+         tri[(i,j)]+=o1
+      else:
+         tri[(i,j)]+=o2
+
+print tri[(0,0)]
