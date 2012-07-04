f009a01505ba9b8416e8035f0f34a67376fe497a
Evan Ackmann <evansbee@gmail.com>
2012-07-03 20:04:14 -0400

    26 done

--- ex26.py
+++ ex26.py
@@ -1,8 +1,29 @@
-from decimal import Decimal
 
+def GetInvFactors(num):
+   remainders = [1]
+   answer = []
+
+   while remainders[-1] and remainders[-1] != 0:
+      answer.append(int(remainders[-1]/num))
+      new_rem = (remainders[-1] - answer[-1] * num) * 10 
+      if remainders.count(new_rem)>0:
+         return answer,remainders.index(new_rem)+1
+      if answer[-1] == 0:
+         remainders[-1] = remainders[-1] * 10
+      else:
+         remainders.append(new_rem)
+
+   return answer, None
+
+
+
+
+repeats,repeatnum = 0,0
 seq = []
 for d in range(1,1001):
-   seq.append(Decimal(1.0)/Decimal(d))
-   print seq[-1]
+   factors,repeat = GetInvFactors(d)
+   if repeat != None:
+      if (len(factors)-repeat) > repeats:
+         repeats,repeatnum = len(factors)-repeat,d
 
-
+print "%d has %d repeating digits." %(repeatnum,repeats)
